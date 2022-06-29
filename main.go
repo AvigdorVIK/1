11ddf5ad705fef280812ec272122278825546852
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-06-29 13:13:25 +0300

    pochatok robory is shablonom

--- main.go
+++ main.go
@@ -2,30 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
 }
 
 func (u User) getAllInto() string {
 	return fmt.Sprintf("User name is: %s. He is %d and has money "+
-		" equal: %d", u.name, u.age, u.money)
+		" equal: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8}
-	bob.setNewName("Alex")
-	fmt.Fprintf(w, bob.getAllInto())
-
+	bob := User{Name: "Bob", Age: 25, Money: -50, Avg_grades: 4.2, Happiness: 0.8}
+	//bob.setNewName("Alex")
+	//fmt.Fprintf(w, bob.getAllInto())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 
 func contacts_page(page http.ResponseWriter, r *http.Request) {
